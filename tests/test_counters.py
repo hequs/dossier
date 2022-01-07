f7f938bd767576e98efd7660525f4b5d9e19676a
Vadim Lyubanenko <lubanenko@yandex.ru>
2022-01-07 14:29:15 +0300

    fix tests

--- tests/test_counters.py
+++ tests/test_counters.py
@@ -1,6 +1,6 @@
 import unittest
 
-from counters import BaseCT, BaseOT, Counters, counter_cosine, RT
+from counters import *
 
 
 class CT(BaseCT):
@@ -9,9 +9,6 @@ class CT(BaseCT):
 
 class OT(BaseOT):
     TEST = 1
-    APPLE = 2
-    ORANGE = 3
-    CHERRY = 4
 
 
 def _days_to_seconds(d):
@@ -61,7 +58,7 @@ class TestCountersOps(unittest.TestCase):
         counters_1.update(OT.TEST, CT.TEST, RT.D30, 'orange', 1, 1)
         counters_2 = Counters()
         counters_2.update(OT.TEST, CT.TEST, RT.D30, 'cherry', 1, 1)
-        value = counter_cosine(counters_1, OT.ORANGE, CT.TEST, counters_2, OT.CHERRY, CT.TEST, RT.D30, 1)
+        value = counter_cosine(counters_1, OT.TEST, CT.TEST, counters_2, OT.TEST, CT.TEST, RT.D30)
         self.assertEqual(value, 0)
 
     def test_cosine(self):
@@ -71,7 +68,7 @@ class TestCountersOps(unittest.TestCase):
         counters_2 = Counters()
         counters_2.update(OT.TEST, CT.TEST, RT.D30, 'apple', 1, 1)
         counters_2.update(OT.TEST, CT.TEST, RT.D30, 'cherry', 1, 1)
-        value = counter_cosine(counters_1, OT.TEST, CT.TEST, counters_2, OT.TEST, CT.TEST, RT.D30, 1)
+        value = counter_cosine(counters_1, OT.TEST, CT.TEST, counters_2, OT.TEST, CT.TEST, RT.D30)
         self.assertAlmostEqual(value, 0.5)
 
     def test_cosine_fails(self):
@@ -83,5 +80,5 @@ class TestCountersOps(unittest.TestCase):
         counters_2.update(OT.TEST, CT.TEST, RT.D30, 'cherry', 1, 5)
 
         def callable():
-            counter_cosine(counters_1, OT.TEST, CT.TEST, counters_2, OT.TEST, CT.TEST, RT.D30, 1)
+            counter_cosine_at(counters_1, OT.TEST, CT.TEST, counters_2, OT.TEST, CT.TEST, RT.D30, 1)
         self.assertRaises(AssertionError, callable)
